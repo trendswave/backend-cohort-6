369a62a4b050c32fe25437a28f64bd1b17145b0c
trendswave <paulchinedumicheal@gmail.com>
2026-03-17 17:17:49 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,24 @@
+// var myName = "Micheal";
+
+// var myAge = 28;
+// myName = "John";
+
+
+// console.log(myName);
+
+// myHeight = 6.5
+
+// console.log(myHeight);
+// console.log(myAge);
+let myString = "Hello World";
+let myNumber = 42;
+let myBoolean = true;
+console.log(myString);
+console.log(myNumber);
+console.log(myBoolean); 
+
+//Non-primitive data types
+let myArray = ["rice", "beans", "corn" , 2 , true ,["john", "jane"], { name: "Alice", age: 30 }];
+let myObject = { name: "Alice", age: 30  , isStudent: false };
+console.log(myArray);
+console.log(myObject); 
---
+++ operators.js
@@ -0,0 +1,47 @@
+//Operator
+//Arithmetic Operators
+let a = 10;
+let b = 5;  
+let c = a + b; // Addition
+let d = a - b; // Subtraction
+let e = a * b; // Multiplication
+let f = a / b; // Division
+let g = a % b; // Modulus
+let k = "Hello Trendswave " + "You need"+ " to learn JavaScript fast"; // String concatenation
+console.log("Addition: " + c);
+console.log("Subtraction: " + d);
+console.log("Multiplication: " + e);
+console.log("Division: " + f);
+console.log("Modulus: " + g);
+console.log("String concatenation: " + k);
+
+
+//comparison operators 
+let x = 10;
+let y = 20; 
+let z = x == y; // Equal to
+let u = x !== y; // Not equal to
+let v = x > y; // Greater than
+let w = x < y;  // Less than
+let m = x >= y; // Greater than or equal to
+let n = x <= y; // Less than or equal to            
+console.log("Equal to: " + z);
+console.log("Not equal to: " + u);
+console.log("Greater than: " + v);
+console.log("Less than: " + w);
+console.log("Greater than or equal to: " + m);
+console.log("Less than or equal to: " + n);
+
+
+// asignment operators
+let p = 10;
+p += 5; // p = p + 5
+console.log(+ p);
+p -= 3; // p = p - 3
+console.log(+ p);
+p *= 2; // p = p * 2
+console.log(+ p);
+p /= 4; // p = p / 4
+console.log(+ p);
+p %= 3; // p = p % 3
+console.log("The final value of p: " + p);
